e1e800c75fee3108726d797d033afa5113b37f8c
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-11-06 13:49:34 -0500

    Xmonad tmux env

--- tmux-envs/pre/xmonad.sh
+++ tmux-envs/pre/xmonad.sh
@@ -1,3 +1,3 @@
-tmux send-keys -t xmonad "vim ~/.xmonad/xmonad.hs ~/.xmobarrc"
-tmux split-window -t xmonad -h
+tmux send-keys -t xmonad "vim ~/.xmonad/xmonad.hs ~/.xmobarrc" "Enter"
+tmux split-window -t xmonad -h -c '#{pane_current_path}'
 tmux select-pane -t xmonad:0.0

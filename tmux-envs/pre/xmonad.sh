ff745275f630194e04a004341b7fd5d049586a6f
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-11-09 01:27:54 -0500

    changeWSifCurrEmpty

--- tmux-envs/pre/xmonad.sh
+++ tmux-envs/pre/xmonad.sh
@@ -1,3 +1,5 @@
-tmux send-keys -t xmonad "vim ~/.xmonad/xmonad.hs ~/.xmobarrc" "Enter"
+tmux send-keys -t xmonad "vim ~/.xmonad/xmonad.hs ~/.xmobarrc /usr/share/xmonad/man/xmonad.hs" "Enter"
 tmux split-window -t xmonad -h -c '#{pane_current_path}'
+tmux split-window -t xmonad -v -c '#{pane_current_path}'
+tmux send-keys -t xmonad:0.2 "ghci" "Enter" ":! clear" "Enter"
 tmux select-pane -t xmonad:0.0

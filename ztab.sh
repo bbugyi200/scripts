7a6713d1906353f28efc317771f77bdf8d05364f
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-11-11 18:17:03 -0500

    Removed unused items

--- ztab.sh
+++
@@ -1,6 +0,0 @@
-switch-or-run "tabbed" "tabbed" "5"
-until wmctrl -lx | grep "tabbed.tabbed"; do
-    sleep 0.5
-done
-wmctrl -r :ACTIVE:
-zathura -e `wmctrl -lx | grep "tabbed.tabbed" | cut -d' ' -f1` "$1"

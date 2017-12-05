4e6d764dd88919e2fef64ef92381a5b6d69539f7
Bryan Bugyi <bryanbugyi34@gmail.com>
2017-12-05 12:23:54 -0500

    Added dropbox status to xmobar

--- tmux-envs/pre/xmonad.sh
+++ tmux-envs/pre/xmonad.sh
@@ -1,7 +1,7 @@
 tmux send-keys -t xmonad "vim ~/.xmonad/xmonad.hs ~/.xmobarrc /usr/share/xmonad/man/xmonad.hs" "Enter"
 tmux split-window -t xmonad -h -c '#{pane_current_path}'
-tmux send-keys -t xmonad:0.1 "clear && tree" "Enter"
-tmux split-window -t xmonad -v -c '#{pane_current_path}'
+tmux send-keys -t xmonad:0.1 "tree" "Enter"
+tmux split-window -t xmonad -v -c '/home/bryan/.xmonad'
 tmux resize-pane -t xmonad:0.2 -D 10
-tmux send-keys -t xmonad:0.2 "ghci" "Enter" ":! clear" "Enter"
+tmux send-keys -t xmonad:0.2 "tree" "Enter"
 tmux select-pane -t xmonad:0.0

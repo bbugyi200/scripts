d5759ea4e041923f98887c7ba8c995075a6386de
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-04-30 05:57:17 -0400

    Add: python template.py

---
+++ templates/template.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import os
+
+from systemd.journal import JournalHandler
+
+log = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    formatter = logging.Formatter('(%(asctime)s) [%(levelname)s] %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    sh.setLevel(logging.ERROR)
+    jh = JournalHandler()
+    jh.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+    jh.setLevel(logging.INFO)
+    fh = logging.FileHandler('/var/tmp/{}.log'.format(os.path.basename(__file__)))
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+    log.setLevel(logging.DEBUG)
+    log.addHandler(sh)
+    log.addHandler(jh)
+    log.addHandler(fh)
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-d', '--debug', action='store_true', help='enable debugging mode')
+    args = parser.parse_args()
+
+    if args.debug:
+        for handler in log.handlers:
+            handler.setLevel(logging.DEBUG)

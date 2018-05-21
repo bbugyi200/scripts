fd05b5ce4830f7598af29d326b0793f8048f57ea
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-21 19:16:44 -0400

    Sty: added docstrings to gutils.subprocess module

--- pymodules/gutils/g_subprocess.py
+++ pymodules/gutils/g_subprocess.py
@@ -7,13 +7,25 @@ import gutils.shared as shared
 
 
 def shell(cmd, cast=str):
-    """ Run Shell Command """
+    """ Run Shell Command
+
+    Args:
+        cmd (str): command.
+        cast (type): type-cast function.
+
+    Returns:
+        Output of shell command, casted to the desired type using the @cast function.
+    """
     out = sp.check_output(cmd, shell=True)
     return cast(out.decode().strip())
 
 
 def notify(*args):
-    """ Sends Desktop Notification """
+    """ Sends Desktop Notification
+
+    Args:
+        *args: arguments to be passed to the notify-send command.
+    """
     assert len(args) > 0, 'No notification message specified.'
 
     cmd_list = ['notify-send']

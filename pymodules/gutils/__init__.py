4a58ed1b74281c1a5b01483d149496f02a2e88f6
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-05-24 14:06:05 -0400

    Add: gutils.functools.compose

--- pymodules/gutils/__init__.py
+++ pymodules/gutils/__init__.py
@@ -15,4 +15,5 @@ import gutils.g_logging as logging  # noqa: F401
 import gutils.g_xdg as xdg  # noqa: F401
 import gutils.g_debug as debug  # noqa: F401
 import gutils.g_subprocess as subprocess  # noqa: F401
+import gutils.g_functools as functools  # noqa: F401
 from gutils.core import *  # noqa: F401, F403

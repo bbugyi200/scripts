f5e8627bf6f3d494754dd5d80d1dc55c9d76f53a
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-09-15 16:51:56 -0400

    Update scripts

--- templates/gutils.sh
+++ templates/gutils.sh
@@ -1,8 +1,31 @@
-########################################
-#  Utility Functions for Bash Scripts  #
-########################################
+###############################################
+#  Global Utility Functions for Bash Scripts  #
+###############################################
 
 function die() {
-    echo "$1" | tee >(logger -t "$(basename "$0")")
-    exit "$2"
+    if [[ -z "$1" ]]; then
+        echo "usage: die MSG [EXIT_CODE]"
+        exit 2
+    fi
+
+    MSG="$1"; shift
+
+    if [[ -n "$1" ]]; then
+        EC="$1"
+    else
+        EC=1
+    fi
+
+    echo "$MSG" | tee >(logger -t "$(basename "$0")")
+    exit "$EC"
+}
+
+function emsg() {
+    if [[ -z "$1" ]]; then
+        echo "usage: emsg MSG"
+        exit 2
+    fi
+
+    MSG="$1"; shift
+    echo ">>> $MSG"
 }

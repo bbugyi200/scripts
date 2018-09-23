14f79ed8269be605f5d000ce5922e42242161d20
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-09-23 04:24:05 -0400

    Docs: fixed gutils directory in top comments

--- modules/bash/secret.sh
+++ modules/bash/secret.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+###################################################################################################
+#  Used to Ensure that a Helper Script is Not Called Directly.                                    #
+#                                                                                                 #
+# Expects the primary script to create a random hash  and more("secret") and write it to a file   #
+# ("secret_file"). Both the hash and the file must then be sent to the helper script as           #
+# command-line arguments.                                                                         #
+###################################################################################################
+
 secret_file="$1"; shift
 secret="$1"; shift
 if [[ ! -f "${secret_file}" ]] || [[ "${secret}" != "$(cat "${secret_file}")" ]]; then
--- templates/template.sh
+++ templates/template.sh
@@ -2,53 +2,13 @@
 
 
 
-source /home/bryan/Dropbox/scripts/templates/gutils.sh
+# ---------- Modules ----------
+source /home/bryan/Dropbox/scripts/modules/bash/gutils.sh
 
-# ---------- Command-line Arguments ----------
-eval set -- "$(getopt -o "d,h,q" -l "debug,help,quiet" -- "$@")"
+# ---------- Global Variables ----------
+scriptname="$(basename "$0")"
 
-USAGE="usage: $(basename "$0") [-d] [-h] [-q]"
-
-read -r -d '' HELP << EOM
-$USAGE
-EOM
-
-while [[ -n "$1" ]]; do
-    case $1 in
-       -d|--debug )
-           debug=true
-           ;;
-       -h|--help )
-           echo "$HELP"
-           exit 0
-           ;;
-       -q|--quiet )
-           quiet=true
-           ;;
-       -- )
-           shift
-           break
-           ;;
-    esac
-    shift
-done
-
-LOG_FILE=/var/tmp/"$(basename "$0")".log
-if [[ "$debug" = true ]] && [[ "$quiet" = true ]]; then
-    exec > "$LOG_FILE"
-elif [[ "$debug" = true ]]; then
-    exec > >(tee "$LOG_FILE")
-elif [[ "$quiet" = true ]]; then
-    exec > /dev/null
-fi
-
-if [[ "$debug" = true ]] || [[ "$quiet" = true ]]; then
-    exec 2>&1
-fi
-
-if [[ "$debug" = true ]]; then
-    PS4='$LINENO: '
-    set -x
-fi
+# shellcheck disable=SC2034
+usage="usage: ${scriptname} [-d] [-h] [-q]"
 
 # ---------- Main ----------

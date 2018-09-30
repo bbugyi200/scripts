a89461cadbae9724b5a2d71ed46ffd7be6071c26
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-09-30 17:41:48 -0400

    Update scripts

--- modules/bash/secret.sh
+++ modules/bash/secret.sh
@@ -6,11 +6,20 @@
 # Expects the primary script to create a random hash  and more("secret") and write it to a file   #
 # ("secret_file"). Both the hash and the file must then be sent to the helper script as           #
 # command-line arguments.                                                                         #
+#                                                                                                 #
+# The ${secret_wrapper} variable must be set before sourcing secret.sh into the client.    #
 ###################################################################################################
 
-secret_file="$1"; shift
+# shellcheck disable=SC2154
+if [[ -z "${secret_wrapper}" ]]; then
+    echo "The \${secret_wrapper} variable is NOT defined."
+    exit 1
+fi
+
 secret="$1"; shift
+secret_file="/tmp/${secret_wrapper}.secret"
 if [[ ! -f "${secret_file}" ]] || [[ "${secret}" != "$(cat "${secret_file}")" ]]; then
-    echo "ERROR: $(basename "$0") is not meant to be run directly. Use the emanage script!"
+    # shellcheck disable=SC2154
+    echo "ERROR: $(basename "$0") is not meant to be run directly. Use the ${secret_wrapper} script!"
     exit 1
 fi

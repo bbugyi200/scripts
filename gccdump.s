7c45e5e04f2aecec5c067e535b07c8add6e411ea
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-01-26 14:36:05 -0500

    Update bin

--- gccdump.s
+++
@@ -1,4 +0,0 @@
-	.file	""
-	.text
-	.ident	"GCC: (Gentoo 7.3.0-r3 p1.4) 7.3.0"
-	.section	.note.GNU-stack,"",@progbits
---
+++ main/fullscreen
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz

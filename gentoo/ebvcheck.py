c3a84fc6c946c694128b5d36deb581d92d6d25d9
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-07-12 09:21:26 -0400

    Update scripts

--- gentoo/ebvcheck.py
+++ gentoo/ebvcheck.py
@@ -4,20 +4,36 @@ Uses app-portage/euscan to verify that ebuilds in the specified portage overlay
 are up to date.
 """
 
-import argparse
 import json
 import os
 import queue
 import re
+import string
 from subprocess import PIPE, Popen
 import sys
 import threading
 import time
-from typing import Any, Callable, Dict, List, Optional, Sequence, Set
+from typing import Callable, Dict, List, NamedTuple, Optional, Sequence, Set
 
 import gutils
 from loguru import logger as log
 import numpy as np
+import requests
+
+
+KeyFunc = Callable[[str], float]
+RepologyProject = List[Dict[str, str]]
+RepologyProjectGroup = Dict[str, RepologyProject]
+
+
+REPOLOGY = "https://repology.org/api/v1/{}".format
+REPOLOGY_PROJECT_MAP = {
+    "dev-util/ix": "ix",
+    "dev-ruby/xdg": "ruby:xdg",
+    "dev-python/tldr-python-client": "tldr",
+    "dev-python/python-stdlib-list": "python:stdlib-list",
+}
+REPOLOGY_REPO_MAP = {"dev-python/tldr-python-client": "arch"}
 
 
 class POpenError(Exception):
@@ -137,7 +153,7 @@ def main(argv: Sequence[str] = None) -> None:
     args = parse_cli_args(argv)
 
     gutils.logging.configure(__file__, debug=args.debug, verbose=args.verbose)
-    if args.color == 'n':
+    if not args.color:
         for attr in dir(gutils.colorize):
             if not attr.startswith('_'):
                 setattr(gutils.colorize, attr, lambda x: x)
@@ -193,62 +209,69 @@ def main(argv: Sequence[str] = None) -> None:
         )
 
 
-def parse_cli_args(argv: Sequence[str]) -> argparse.Namespace:
-    parser = gutils.ArgumentParser()
+class Arguments(NamedTuple):
+    debug: bool
+    verbose: bool
+    overlay_dir: str
+    max_thread_count: int
+    color: bool
+    live: bool
+    offline: bool
 
-    default: Any = '/var/db/repos/bbugyi'
+
+def parse_cli_args(argv: Sequence[str]) -> Arguments:
+    parser = gutils.ArgumentParser()
     parser.add_argument(
         '-D',
         dest='overlay_dir',
-        default=default,
+        default='/var/db/repos/bbugyi',
         help=(
-            'Location (directory path) of the portage overlay. Defaults to {}.'
-            .format(default)
+            'Location (directory path) of the portage overlay. Defaults to'
+            ' %(default)s.'
         ),
     )
-
-    default = 8
     parser.add_argument(
         '-T',
         '--max-thread-count',
         type=int,
-        default=default,
-        help='Max number of threads to start. Defaults to {}.'.format(default),
+        default=3,
+        help='Max number of threads to start. Defaults to %(default)s.',
     )
-
-    default = 'y'
     parser.add_argument(
         '--color',
         choices=('y', 'n'),
-        default=default,
-        help='Colorize output? Defaults to \'{}\'.'.format(default),
+        default="y",
+        help='Colorize output? Defaults to %(default)r.',
     )
-
-    default = 'n'
     parser.add_argument(
         '--live-builds',
         dest='live',
         action='store',
         choices=('y', 'n'),
-        default=default,
+        default="n",
         help=(
             'Print status for live ebuilds (ebuilds with version 9999).'
-            ' Defaults to \'{}\'.'.format(default)
+            ' Defaults to %(default)r.'
         ),
     )
-
-    default = 'n'
     parser.add_argument(
         '--offline',
         choices=('y', 'n'),
-        default=default,
+        default="n",
         help=(
             'Print status for packages that are not installed on this machine.'
-            ' Defaults to \'{}\'.'.format(default)
+            ' Defaults to %(default)r.'
         ),
     )
 
-    return parser.parse_args(argv[1:])
+    args = parser.parse_args(argv[1:])
+
+    kwargs = dict(args._get_kwargs())
+    kwargs["color"] = args.color == "y"
+    kwargs["live"] = args.live == "y"
+    kwargs["offline"] = args.offline == "y"
+
+    return Arguments(**kwargs)
 
 
 def check_pkg(  # noqa: C901
@@ -277,10 +300,10 @@ def check_pkg(  # noqa: C901
         if not re.match(
             r'[A-z\-\d]+/[A-z\-\d]+-[\d\.]+(_[A-z_]+\d*)?(-r\d*)?$', out
         ):
-            if show_offline_pkgs == 'y':
+            if show_offline_pkgs:
                 message_manager.print(
                     msg_fmt.format(
-                        pkg, gutils.colorize.yellow('NOT INSTALLED')
+                        pkg, gutils.colorize.magenta('NOT INSTALLED')
                     ),
                     id_number,
                 )
@@ -291,7 +314,7 @@ def check_pkg(  # noqa: C901
 
         version = out.split('-')[-1]
         if version == '9999':
-            if show_live_pkgs == 'y':
+            if show_live_pkgs:
                 message_manager.print(
                     msg_fmt.format(pkg, gutils.colorize.blue('LIVE BUILD')),
                     id_number,
@@ -301,7 +324,7 @@ def check_pkg(  # noqa: C901
                 message_manager.noop(id_number)
             return
 
-        latest_version_A = get_latest_version(pkg)
+        latest_version_A = latest_version_B = get_latest_version(pkg)
         if latest_version_A is None:
             log.debug('{} UNKNOWN (#{}).'.format(pkg, id_number))
             message_manager.print(
@@ -310,17 +333,14 @@ def check_pkg(  # noqa: C901
             )
         else:
             if latest_version_A != version:
-                latest_version_B = latest_version_A
-                latest_version_A = get_latest_version(
-                    pkg, key=lambda x: -fuzzy_match(pkg, x), use_cache=True
-                )
+                latest_version_B = get_latest_version(pkg, key=fuzzy_key(pkg))
 
-            if latest_version_A != version:
+            if version not in [latest_version_A, latest_version_B]:
                 log.debug('{} FAILED (#{}).'.format(pkg, id_number))
                 if latest_version_A == latest_version_B:
                     new_version = latest_version_A
                 else:
-                    new_version = f"{latest_version_B} OR {latest_version_A}"
+                    new_version = f"{latest_version_A} OR {latest_version_B}"
 
                 fail_msg = msg_fmt.format(
                     pkg,
@@ -350,47 +370,45 @@ def get_all_files(directory: str) -> List[str]:
 
 
 def get_latest_version(
-    full_pkg: str, key: Callable = len, use_cache: bool = True
+    full_pkg: str, key: KeyFunc = len, disable_cache: bool = False,
 ) -> Optional[str]:
-    import requests
-
-    params = {}
     if "/" in full_pkg:
         (_category, pkg) = full_pkg.split("/", 1)
     else:
         pkg = full_pkg
 
-    params["search"] = pkg
+    pkg_attr = full_pkg.replace("-", "_").replace("/", "__")
 
     self = get_latest_version
-    pkg_attr = full_pkg.replace("-", "_").replace("/", "__")
-    if use_cache and hasattr(self, pkg_attr):
+    if not disable_cache and hasattr(self, pkg_attr):
         data = getattr(self, pkg_attr)
     else:
-        exc = None
-        for _ in range(5):
-            try:
-                resp = requests.get(
-                    "https://repology.org/api/v1/projects/",
-                    params=params,
-                    timeout=2.0,
-                )
-                data = json.loads(resp.text)
-                break
-            except requests.exceptions.ReadTimeout as e:
-                exc = e
-                time.sleep(1)
+        if full_pkg in REPOLOGY_PROJECT_MAP:
+            project_name = REPOLOGY_PROJECT_MAP[full_pkg]
+            data = {project_name: repology_project(project_name)}
         else:
-            assert exc
-            raise exc
+            data = all_repology_projects(pkg)
 
     if not hasattr(self, pkg_attr):
         setattr(self, pkg_attr, data)
 
     result = None
     for pkg_key in sorted(data, key=key):
+        if full_pkg in REPOLOGY_REPO_MAP:
+            repo = REPOLOGY_REPO_MAP[full_pkg]
+            for D in data[pkg_key]:
+                if D['repo'] == repo:
+                    result = D['version']
+                    break
+
+            if result:
+                break
+
         version_set = {
-            D['version'] for D in data[pkg_key] if '.' in D['version']
+            D["version"]
+            for D in data[pkg_key]
+            if "." in D["version"]
+            and set(D["version"]).issubset(set(string.digits + "."))
         }
         result = max(version_set)
         break
@@ -398,10 +416,59 @@ def get_latest_version(
     return result
 
 
-def fuzzy_match(s: str, t: str) -> float:
+def repology_project(name: str) -> RepologyProject:
+    resp = requests.get(REPOLOGY(f"project/{name}"))
+    result = json.loads(resp.text)
+    return result
+
+
+def all_repology_projects(search: str) -> RepologyProjectGroup:
+    result = repology_project_group(search)
+
+    tmp = result
+    while len(tmp) == 200:
+        tmp = repology_project_group(search, name=list(tmp.keys())[-1])
+        result.update(tmp)
+
+    return result
+
+
+def repology_project_group(
+    search: str, name: str = ""
+) -> RepologyProjectGroup:
+    exc = None
+    timeout = 1
+    for _ in range(5):
+        try:
+            resp = requests.get(
+                REPOLOGY(f"projects/{name}"),
+                params={"search": search},
+                timeout=timeout,
+            )
+            result = json.loads(resp.text)
+            break
+        except requests.exceptions.ReadTimeout as e:
+            exc = e
+            timeout *= 2
+            time.sleep(1)
+    else:
+        assert exc
+        raise exc
+
+    return result
+
+
+def fuzzy_key(pkg: str) -> KeyFunc:
+    def _fuzzy_key(P: str) -> float:
+        return -fuzzy_match(pkg, P)
+
+    return _fuzzy_key
+
+
+def fuzzy_match(S: str, T: str) -> float:
     # Initialize matrix of zeros
-    rows = len(s) + 1
-    cols = len(t) + 1
+    rows = len(S) + 1
+    cols = len(T) + 1
     distance = np.zeros((rows, cols), dtype=int)
 
     # Populate matrix of zeros with the indeces of each character of both
@@ -415,7 +482,7 @@ def fuzzy_match(s: str, t: str) -> float:
     # and/or substitutions
     for col in range(1, cols):
         for row in range(1, rows):
-            if s[row - 1] == t[col - 1]:
+            if S[row - 1] == T[col - 1]:
                 # If the characters are the same in the two strings in a given
                 # position [i,j] then the cost is 0
                 cost = 0
@@ -432,8 +499,8 @@ def fuzzy_match(s: str, t: str) -> float:
             )  # Cost of substitutions
 
     # Computation of the Levenshtein Distance Ratio
-    Ratio = ((len(s) + len(t)) - distance[row][col]) / (len(s) + len(t))
-    return Ratio
+    ratio = ((len(S) + len(T)) - distance[row][col]) / (len(S) + len(T))
+    return ratio
 
 
 if __name__ == "__main__":
